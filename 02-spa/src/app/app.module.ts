41ff84ac480af60fa57641231d4817766cd3da46
LA-BESTIA--PABL\pjord <pjorda96@gmail.com>
2019-03-01 20:40:59 +0100

    clase 59

--- 02-spa/src/app/app.module.ts
+++ 02-spa/src/app/app.module.ts
@@ -1,13 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Rutas
 import { APP_ROUTING } from "./app.routes";
+
+// Servicios
+import { HeroesService } from './services/heroes.service'
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { HeroeComponent } from './heroe/heroe.component';
 
 @NgModule({
   declarations: [
@@ -15,13 +20,16 @@ import { HeroesComponent } from './heroes/heroes.component';
     NavbarComponent,
     HomeComponent,
     AboutComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroeComponent
   ],
   imports: [
     BrowserModule,
     APP_ROUTING
   ],
-  providers: [],
+  providers: [
+    HeroesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
