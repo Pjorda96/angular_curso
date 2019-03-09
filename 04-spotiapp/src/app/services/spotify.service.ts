6e19cf2590c07ebdfa9a4c43cc4a51fd17555541
LA-BESTIA--PABL\pjord <pjorda96@gmail.com>
2019-03-09 11:30:42 +0100

    spotiapp

---
+++ 04-spotiapp/src/app/services/spotify.service.ts
@@ -0,0 +1,33 @@
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {map} from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SpotifyService {
+
+  constructor(private http: HttpClient) { }
+
+  getQuery(query: string) {
+    const url = `https://api.spotify.com/v1/${query}`;
+
+    const headers = new HttpHeaders({
+      'Authorization': 'Bearer BQAj4nVDSdBLbDpAfDXNgRQZIkHHRqU2sA4Egiv2W-OuzXTYIeVKd_8melLRGo4WWMOBRe-wRMpRwerzBzo'
+    });
+
+    return this.http.get(url, {headers});
+  }
+
+  getNewReleases() {
+
+    return this.getQuery('browse/new-releases')
+            .pipe( map( data => data['albums'].items ));
+  }
+
+  getArtista( termino: string ) {
+
+    return this.getQuery(`search?q=${ termino }&type=artist&limit=15`)
+      .pipe( map( data => data['artists'].items ));
+  }
+}
